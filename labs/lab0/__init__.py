dcd148aef23b2ddaa8e5130f335be18cabd405a2
Bruce Elgort <bruce.elgort@gmail.com>
2022-08-23 13:26:07 -0700

    Testing

--- labs/lab0/__init__.py
+++ labs/lab0/__init__.py
@@ -9,7 +9,7 @@ def exists():
 
 @check50.check(exists)
 def testbruce():
-    """input of HELLO yields output of hello"""
+    """input of Bruce, the numbers 42 and 11 yields output of Hey Bruce! The sum of 42 and 11 is 53."""
     check50.run("python3 lab0.py").stdin("Bruce", prompt=True).stdin(
         "42", prompt=True
     ).stdin("11", prompt=True).stdout("Hey Bruce! The sum of 42 and 11 is 53.\n").exit()
