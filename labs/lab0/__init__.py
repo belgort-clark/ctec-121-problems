2bc0acc0585f96d92a8f23c9330d901ab3da23d1
Bruce Elgort <bruce.elgort@gmail.com>
2022-08-23 13:29:50 -0700

    Added new tests

--- labs/lab0/__init__.py
+++ labs/lab0/__init__.py
@@ -9,7 +9,25 @@ def exists():
 
 @check50.check(exists)
 def testbruce():
-    """input of Bruce, the numbers 42 and 11 yields output of Hey Bruce! The sum of 42 and 11 is 53."""
+    """input of Bruce, the numbers 42 and 11 yields output of 'Hey Bruce! The sum of 42 and 11 is 53.'"""
     check50.run("python3 lab0.py").stdin("Bruce", prompt=True).stdin(
         "42", prompt=True
     ).stdin("11", prompt=True).stdout("Hey Bruce! The sum of 42 and 11 is 53.\n").exit()
+
+
+def testjacqueline():
+    """input of Jacqueline, the numbers 22 and 0 yields output of 'Hey Jacqueline! The sum of 22 and 0 is 22.'"""
+    check50.run("python3 lab0.py").stdin("Bruce", prompt=True).stdin(
+        "42", prompt=True
+    ).stdin("11", prompt=True).stdout(
+        "Hey Jacqueline! The sum of 22 and 0 is 22.\n"
+    ).exit()
+
+
+def testrashida():
+    """input of Rashida, the numbers -42 and -43 yields output of 'Hey Rashida! The sum of -42 and-43 is -85.'"""
+    check50.run("python3 lab0.py").stdin("Bruce", prompt=True).stdin(
+        "42", prompt=True
+    ).stdin("11", prompt=True).stdout(
+        "Hey Rashida! The sum of -42 and -43 is -85.\n"
+    ).exit()
