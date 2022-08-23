4fef81e9706d4008a3a64bfb03cb483ecc166074
Bruce Elgort <bruce.elgort@gmail.com>
2022-08-23 13:24:15 -0700

    Bug fixes

--- labs/lab0/__init__.py
+++ labs/lab0/__init__.py
@@ -8,20 +8,12 @@ def exists():
 
 
 @check50.check(exists)
-def testhello():
+def testbruce():
     """input of HELLO yields output of hello"""
-    check50.run("python3 lab0.py").stdin("HELLO", prompt=False).stdout("hello").exit()
+    check50.run("python3 lab0.py").stdin("Bruce", prompt=True).stdin(
+        "42", prompt=True
+    ).stdin(
+        "11", prompt=True
+    ).
+    stdout("Hey Bruce! The sum of 42 and 11 is 53.\n").exit()
 
-
-@check50.check(exists)
-def testcs50():
-    """input of THIS IS CS50 yields output of this is cs50"""
-    check50.run("python3 indoor.py").stdin("THIS IS CS50", prompt=False).stdout(
-        "this is cs50"
-    ).exit()
-
-
-@check50.check(exists)
-def testnumber():
-    """input of 50 yields output of 50"""
-    check50.run("python3 indoor.py").stdin("50", prompt=False).stdout("50").exit()
