c77f446db177ab53948ef7fddb39e3c6364fb6ba
Bruce Elgort <bruce.elgort@gmail.com>
2022-08-23 18:28:09 -0700

    Fixed bug

--- labs/lab1/__init__.py
+++ labs/lab1/__init__.py
@@ -13,5 +13,5 @@ def testviktor():
     check50.run("python3 lab1.py").stdin("Viktor", prompt=True).stdin(
         "15.25", prompt=True
     ).stdin("38", prompt=True).stdout(
-        "Name: Viktor\nHourly wage: $15.25\nLocal taxes: $57.95\nMedical insurance: $69.54\nOvertime pay: $0\nTotal gross earnings: $579.5\nNet pay: $452.01\n"
+        "\nName: Viktor\nHourly wage: $15.25\nLocal taxes: $57.95\nMedical insurance: $69.54\nOvertime pay: $0\nTotal gross earnings: $579.5\nNet pay: $452.01\n"
     ).exit()
