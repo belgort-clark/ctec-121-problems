5373b4c2c9aa442433c2f7148699234d5c347ce2
Bruce Elgort <bruce.elgort@gmail.com>
2022-08-23 20:01:24 -0700

    test

--- labs/lab1/__init__.py
+++ labs/lab1/__init__.py
@@ -12,6 +12,6 @@ def testviktor():
     """input of Viktor, hourly wage 15.25, hours worked 38, yields Net Pay of $452.01'"""
     check50.run("python3 lab1.py").stdin("Viktor", prompt=True).stdin(
         "15.25", prompt=True
-    ).stdin("38", prompt=True).stdout("\n").stdout(
+    ).stdin("38", prompt=True).stdout(
         "Name: Viktor\nHourly wage: $15.25\nLocal taxes: $57.95\nMedical insurance: $69.54\nOvertime pay: $0\nTotal gross earnings: $579.5\nNet pay: $452.01\n"
     ).exit()
