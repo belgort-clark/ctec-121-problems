574aef3e65870f99b2f59c441cb616bafe2054e2
Bruce Elgort <belgort@clark.edu>
2025-02-03 14:51:26 -0800

    Ready to grade

--- week_5_tuesday_files/__init__.py
+++ week_5_tuesday_files/__init__.py
@@ -3,5 +3,8 @@ import check50
 @check50.check()
 def exists():
     """problem1.py, problem2.py, problem3.py and problem4.py exist"""
-    check50.exists("*.py")
+    check50.exists("demo1.py")
+    check50.exists("demo2.py")
+    check50.exists("demo3.py")
+    check50.exists("demo4.py")
 
