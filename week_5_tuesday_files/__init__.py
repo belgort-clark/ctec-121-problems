c183d8193a8131d23aa4e0594c0cc1b222066674
Bruce Elgort <belgort@clark.edu>
2025-02-03 14:49:17 -0800

    Minor updates

--- week_5_tuesday_files/__init__.py
+++ week_5_tuesday_files/__init__.py
@@ -1,10 +1,7 @@
 import check50
 
-# @check50.check()
-# def exists():
-#     """problem1.py, problem2.py, problem3.py and problem4.py exist"""
-#     check50.exists("problem1.py")
-#     check50.exists("problem2.py")
-#     check50.exists("problem3.py")
-#     check50.exists("problem4.py")
+@check50.check()
+def exists():
+    """problem1.py, problem2.py, problem3.py and problem4.py exist"""
+    check50.exists("*.py")
 
